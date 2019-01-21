694fd3ef85d4411bf3f0b4b70aa3c1e541bc1125
Dmitry Vansovich <40145478+Shawn0592@users.noreply.github.com>
2019-01-21 21:31:50 +0300

    v0.1, часть 1

---
+++ heroes/info.js
@@ -0,0 +1,159 @@
+var HEROES = [
+	{
+		"name": "Ифрит",
+		"type": "fire",
+		"img": "ifrit",
+
+		"max-level": 22,
+		"rare": false,
+
+		"params": [
+			5, 3, 10, 7, 10,
+			5, 3, 7, 5, 5, 2, 3
+		]
+	},{
+		"name": "Горгулья",
+		"type": "fire",
+		"img": "gargoyle",
+
+		"max-level": 15,
+		"rare": false,
+
+		"params": [
+			4, 4, 6, 8, 10,
+			8, 10, 5, 7, 4, 3, 4
+		]
+	},{
+		"name": "Огонь",
+		"type": "fire",
+		"img": "unfriendly-fire",
+
+		"max-level": 6,
+		"rare": false,
+
+		"params": [
+			7, 5, 10, 10, 6,
+			6, 7, 10, 9, 10, 5, 3
+		]
+	},{
+		"name": "Ледяной дракон",
+		"type": "winter",
+		"img": "dragon-head",
+
+		"max-level": 9,
+		"rare": true,
+
+		"params": [
+			6, 2, 9, 8, 10,
+			10, 9, 8, 7, 8, 4, 3
+		]
+	},{
+		"name": "Ледяной голем",
+		"type": "winter",
+		"img": "ice-golem",
+
+		"max-level": 15,
+		"rare": false,
+
+		"params": [
+			7, 1, 7, 7, 10,
+			9, 10, 8, 8, 7, 4, 2
+		]
+	},{
+		"name": "Голем",
+		"type": "winter",
+		"img": "golem-head",
+
+		"max-level": 32,
+		"rare": false,
+
+		"params": [
+			6, 2, 6, 7, 10,
+			8, 10, 6, 6, 6, 3, 2
+		]
+	},{
+		"name": "Смертельный голем",
+		"type": "chemical",
+		"img": "death-zone",
+
+		"max-level": 7,
+		"rare": false,
+
+		"params": [
+			5, 7, 7, 5, 9,
+			7, 7, 9, 8, 8, 2, 4
+		]
+	},{
+		"name": "Кенку",
+		"type": "chemical",
+		"img": "kenku",
+
+		"max-level": 5,
+		"rare": true,
+
+		"params": [
+			7, 10, 10, 10, 3,
+			10, 8, 10, 9, 2, 7, 2
+		]
+	},{
+		"name": "Смерть",
+		"type": "chemical",
+		"img": "grim-reaper",
+
+		"max-level": 7,
+		"rare": false,
+
+		"params": [
+			2, 9, 8, 10, 3,
+			8, 7, 10, 10, 5, 3, 3
+		]
+	},{
+		"name": "Череп",
+		"type": "chemical",
+		"img": "half-dead",
+
+		"max-level": 5,
+		"rare": false,
+
+		"params": [
+			3, 10, 10, 10, 2,
+			10, 10, 5, 10, 10, 4, 1
+		]
+	},{
+		"name": "Коварное дерево",
+		"type": "druid",
+		"img": "evil-tree",
+
+		"max-level": 8,
+		"rare": false,
+
+		"params": [
+			9, 3, 2, 8, 8,
+			9, 8, 8, 8, 5, 3, 5
+		]
+	},{
+		"name": "Рогатая смерть",
+		"type": "druid",
+		"img": "horned-reptile",
+
+		"max-level": 9,
+		"rare": false,
+
+		"params": [
+			10, 10, 10, 10, 10,
+			3, 2, 4, 5, 3, 3, 2
+		]
+	},{
+		"name": "Выбор судьбы",
+		"type": "druid",
+		"img": "pick-of-destiny",
+
+		"max-level": 3,
+		"rare": true,
+
+		"params": [
+			10, 10, 10, 10, 10,
+			10, 10, 10, 10, 10, 1, 1
+		]
+	}
+];
---
+++ heroes/magic.js
@@ -0,0 +1,38 @@
+var MAGIC = {
+	"fire":
+		[
+			{"name": "Удар топором", "desc": "none", "damage": 21, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Восстановление огненных ран", "desc": "none", "damage": 0, "magic_damage": 0, "add_magic": 20, "add_heal": 20},
+			{"name": "Ифритовая казнь", "desc": "none", "damage": 0, "magic_damage": 40, "add_magic": 0, "add_heal": 0},
+			{"name": "Метеор", "desc": "none", "damage": 50, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Столб огня", "desc": "none", "damage": 0, "magic_damage": 50, "add_magic": 0, "add_heal": 0},
+			{"name": "Дымовая завеса", "desc": "none", "damage": 10, "magic_damage": 0, "add_magic": 10, "add_heal": 10}
+		],
+	"winter":
+		[
+			{"name": "Смертельная температура", "desc": "none", "damage": 21, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Морозная регенерация", "desc": "none", "damage": 0, "magic_damage": 0, "add_magic": 20, "add_heal": 20},
+			{"name": "Рычанье грёз", "desc": "none", "damage": 0, "magic_damage": 40, "add_magic": 0, "add_heal": 0},
+			{"name": "Кристальный обстрел", "desc": "none", "damage": 50, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Объятия голема", "desc": "none", "damage": 0, "magic_damage": 50, "add_magic": 0, "add_heal": 0},
+			{"name": "Крушение льда", "desc": "none", "damage": 10, "magic_damage": 0, "add_magic": 10, "add_heal": 10}
+		],
+	"chemical":
+		[
+			{"name": "Смертельные стрелы", "desc": "none", "damage": 21, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Заклинание тьмы", "desc": "none", "damage": 0, "magic_damage": 0, "add_magic": 20, "add_heal": 20},
+			{"name": "Сила молнии", "desc": "none", "damage": 0, "magic_damage": 40, "add_magic": 0, "add_heal": 0},
+			{"name": "Жертва дьяволу", "desc": "none", "damage": 50, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Обратно в могилу", "desc": "none", "damage": 0, "magic_damage": 50, "add_magic": 0, "add_heal": 0},
+			{"name": "Поглощение", "desc": "none", "damage": 10, "magic_damage": 0, "add_magic": 10, "add_heal": 10}
+		],
+	"druid":
+		[
+			{"name": "Кара природы", "desc": "none", "damage": 21, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Поглощение жизненной энергии", "desc": "none", "damage": 0, "magic_damage": 0, "add_magic": 20, "add_heal": 20},
+			{"name": "Ответ природы", "desc": "none", "damage": 0, "magic_damage": 40, "add_magic": 0, "add_heal": 0},
+			{"name": "Пронзающая стрела", "desc": "none", "damage": 50, "magic_damage": 0, "add_magic": 0, "add_heal": 0},
+			{"name": "Обрушение земли", "desc": "none", "damage": 0, "magic_damage": 50, "add_magic": 0, "add_heal": 0},
+			{"name": "Смертельный порог", "desc": "none", "damage": 10, "magic_damage": 0, "add_magic": 10, "add_heal": 10}
+		]
+};
